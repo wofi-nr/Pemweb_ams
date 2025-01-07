d897230a746e9a173a02b1e737ddaefa994b1e46
Nailu12 <nailunurrohmah12@gmail.com>
2025-01-07 18:20:32 +0700

    Membuat Tampilan Data siswa dan sub menunya Fixes #15

---
+++ resources/views/view_data_siswa.blade.php
@@ -0,0 +1,175 @@
+@extends('layouts.app')
+
+@section('title', 'View Data Siswa')
+
+@section('content')
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <a href="#" class="brand-link">
+            <img src="https://via.placeholder.com/150" alt="Logo" class="brand-image img-circle elevation-3">
+            <span class="brand-text font-weight-light">AMS Admin</span>
+        </a>
+        <div class="sidebar">
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+                    <li class="nav-item">
+                        <a href="dashboard.html" class="nav-link ">
+                            <i class="nav-icon fas fa-tachometer-alt"></i>
+                            <p>Dashboard</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link ">
+                            <i class="nav-icon fas fa-school"></i>
+                            <p>
+                                Data Kelas
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="input_kelas.html" class="nav-link ">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Input Kelas</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="view_kelas.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>View Data Kelas</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link  active">
+                            <i class="nav-icon fas fa-user-graduate"></i>
+                            <p>
+                                Data Siswa
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="input_siswa.html" class="nav-link ">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Input Siswa</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="view_siswa.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>View Data Siswa</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link ">
+                            <i class="nav-icon fas fa-money-bill-wave"></i>
+                            <p>
+                                Pembayaran
+                                <i class="right fas fa-angle-left"></i>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="pembayaran_spp.html" class="nav-link ">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Pembayaran SPP</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="view_data_spp.html" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>View Data Pembayaran</p>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+                
+                    <li class="nav-item">
+                        <a href="setting_app.html" class="nav-link">
+                            <i class="nav-icon fas fa-cogs"></i>
+                            <p>Setting</p>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </aside>
+
+    <!-- Content Wrapper -->
+    <div class="content-wrapper">
+        <div class="content-header">
+            <div class="container-fluid">
+                <h1 class="m-0">View Data Siswa</h1>
+            </div>
+        </div>
+        <div class="content">
+            <div class="container-fluid">
+                <div class="card">
+                    <div class="card-header">
+                        <h3 class="card-title">Daftar Siswa</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                <i class="fas fa-minus"></i>
+                            </button>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <!-- Filter Search -->
+                        <div class="row mb-3">
+                            <div class="col-md-4">
+                                <input type="text" id="searchNIS" class="form-control" placeholder="Cari berdasarkan NIS">
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" id="searchKelas" class="form-control" placeholder="Cari berdasarkan Kelas">
+                            </div>
+                        </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
+                                <tr>
+                                    <th>No</th>
+                                    <th>Nama Siswa</th>
+                                    <th>NIS</th>
+                                    <th>Kelas</th>
+                                    <th>Alamat</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>Ahmad</td>
+                                    <td>123456</td>
+                                    <td>VII IPA 1</td>
+                                    <td>Jl. Merpati No. 2</td>
+                                    <td>
+                                        <button class="btn btn-info btn-sm"><i class="fas fa-edit"></i> Edit</button>
+                                        <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i> Hapus</button>
+                                    </td>
+                                </tr>
+                                <tr>
+                                    <td>2</td>
+                                    <td>Siti</td>
+                                    <td>654321</td>
+                                    <td>VI IPA 2</td>
+                                    <td>Jl. Kenari No. 5</td>
+                                    <td>
+                                        <button class="btn btn-info btn-sm"><i class="fas fa-edit"></i> Edit</button>
+                                        <button class="btn btn-danger btn-sm"><i class="fas fa-trash"></i> Hapus</button>
+                                    </td>
+                                </tr>
+                                <!-- Tambahkan data siswa lainnya di sini -->
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
