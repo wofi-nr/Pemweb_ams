efbc8f4e66bb67469a158c0dc7548c0ac0a885d5
wofi-nr <wofinr@gmail.com>
2025-01-09 20:02:39 +0700

    Konfigurasi Pengalamatan Menu dan Revisi Tampilan Fixes #27

--- resources/views/edit_data_siswa.blade.php
+++ resources/views/edit_data_siswa.blade.php
@@ -3,52 +3,6 @@
 @section('title', 'EditDataSiswa')
 
 @section('content')
-
-<!-- Main Sidebar Container -->
-<aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="#" class="brand-link">
-                <img src="https://via.placeholder.com/150" alt="Logo" class="brand-image img-circle elevation-3">
-                <span class="brand-text font-weight-light">AMS Admin</span>
-            </a>
-            <div class="sidebar">
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <li class="nav-item">
-                            <a href="dashboard.html" class="nav-link ">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p>Dashboard</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link active">
-                                <i class="nav-icon fas fa-user-graduate"></i>
-                                <p>
-                                    Data Siswa
-                                    <i class="right fas fa-angle-left"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="input_siswa.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Input Siswa</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="edit_siswa.html" class="nav-link active">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Edit Data Siswa</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </aside>
-
-        <!-- Content Wrapper -->
-        <div class="content-wrapper">
             <!-- Content Header -->
             <div class="content-header">
                 <div class="container-fluid">
@@ -104,6 +58,4 @@
                     </div>
                 </div>
             </section>
-        </div>
-        <!-- /.content-wrapper -->
 @endsection
--- resources/views/setting_th_ajaran.blade.php
+++ resources/views/setting_th_ajaran.blade.php
@@ -3,58 +3,6 @@
 @section('title', 'Setting Tahun Ajaran')
 
 @section('content') 
-        
-        <!-- Main Sidebar Container -->
-        <aside class="main-sidebar sidebar-dark-primary elevation-4">
-            <a href="#" class="brand-link">
-                <img src="https://via.placeholder.com/150" alt="Logo" class="brand-image img-circle elevation-3">
-                <span class="brand-text font-weight-light">AMS Admin</span>
-            </a>
-            <div class="sidebar">
-                <nav class="mt-2">
-                    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                        <li class="nav-item">
-                            <a href="dashboard.html" class="nav-link">
-                                <i class="nav-icon fas fa-tachometer-alt"></i>
-                                <p>Dashboard</p>
-                            </a>
-                        </li>
-                        <li class="nav-item">
-                            <a href="#" class="nav-link active">
-                                <i class="nav-icon fas fa-cogs"></i>
-                                <p>
-                                    Pengaturan
-                                    <i class="right fas fa-angle-left"></i>
-                                </p>
-                            </a>
-                            <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="setting_app.html" class="nav-link ">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Pengaturan Umum</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="setting_th_ajaran.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Pengaturan Th Ajaran</p>
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="login.html" class="nav-link">
-                                        <i class="far fa-circle nav-icon"></i>
-                                        <p>Logout</p>
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-        </aside>
-
-        <!-- Content Wrapper -->
-        <div class="content-wrapper">
             <!-- Content Header -->
             <div class="content-header">
                 <div class="container-fluid">
@@ -113,5 +61,4 @@
                     </div>
                 </div>
             </section>
-        </div>
         @endsection
