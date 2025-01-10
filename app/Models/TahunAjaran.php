a07bf4e16783e2c4b2bab97d9441268603aaa4c3
rizkiana13 <rizkianadm13@gmail.com>
2025-01-10 10:56:29 +0700

    Membuat Models Tahun Ajaran Fixes #5

--- app/Models/TahunAjaran.php
+++ app/Models/TahunAjaran.php
@@ -1,4 +1,4 @@
-// app/Models/TahunAjaran.php
+<?php
 
 namespace App\Models;
 
@@ -9,4 +9,5 @@ class TahunAjaran extends Model
     protected $table = 'data_tahun_ajaran';
     protected $fillable = ['tahun_ajaran', 'status', 'tgl_tambah'];
     public $timestamps = false; // Jika Anda ingin menggunakan current_timestamp()
+ //
 }
