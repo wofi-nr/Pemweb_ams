551a0099963bc9ec134c3d621fb1bc54b9dd7749
MuhammadAnisNurFauzi <yukkperubahan@gmail.com>
2025-01-10 10:24:57 +0700

    Membuat Models Pembayaran

---
+++ app/Models/Pembayaran.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pembayaran extends Model
+{
+    use HasFactory;
+
+    protected $table = 'data_pembayaran';
+
+    protected $primaryKey = 'id_pembayaran';
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'tahun_ajaran',
+        'nis',
+        'bulan',
+        'jumlah_bayar',
+        'tgl_bayar',
+        'status_bayar',
+    ];
+}
